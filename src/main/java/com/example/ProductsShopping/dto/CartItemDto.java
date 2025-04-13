b29490b3ec7bbf7c5c8122de64ed6f2260035a21
flonkerr <im.a.cool.croissant@gmail.com>
2025-04-13 11:28:13 +0400

    add dto

---
+++ src/main/java/com/example/ProductsShopping/dto/CartItemDto.java
@@ -0,0 +1,16 @@
+package com.example.ProductsShopping.dto;
+
+import lombok.Data;
+@Data
+public class CartItemDto {
+    private Long id;
+    private Long productId;
+    private String brand;
+    private String model;
+    private String category;
+    private String description;
+    private double price;
+    private double rate;
+    private String imageUrl;
+    private Integer quantity;
+}
